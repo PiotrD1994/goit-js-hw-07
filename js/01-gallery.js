738a0badfc87281b6a4dacba80d5b77e3694b1b1
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-08-07 20:23:24 +0200

    HW 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector('.gallery');
-const galleryItemsHTML = galleryItems.map(({preview, original, descritpion}) => {
-    return `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${descritpion}"/>
-    </a>
-    </li>`
-}).join('');
+console.log(galleryItems);
+ 
+const gallery = document.querySelector('.gallery')
 
-gallery.innerHTML = galleryItemsHTML
-
-gallery.addEventListener('click', galleryClick)
-
-function galleryClick(event) {
-    event.preventDefault();
-    const clickElement = event.target;
-    if (clickElement.classList.contains("gallery__image")) {
-        const largeImage = clickElement.dataset.source
-
-       const instance = basicLightbox.create (
-        `<img src= "${largeImage}" width="800" height="600">`
-       )
-       instance.show();
-
-       window.addEventListener('keydown', closeModal)
-
-       function closeModal(event) {
-        if(event.key === 'Escape') {
-            instance.close()
-            window.removeEventListener('keydown', closeModal)
-        }
-       }
-    }
+for (const item of galleryItems) {
+const htmlString =   `<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img 
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="large-image.jpg"
+    alt="${item.description}"
+  />
+</a>
+</div>`
+gallery.innerHTML += htmlString
 }
+ 
+const gallerypictrues = document.querySelectorAll('.gallery__item')
+const galleryLinks = document.querySelectorAll('.gallery__link')
+const galleryImages = document.querySelectorAll('.gallert__image')
+
+galleryItems.addEventListener('click', imageSelect)
+
+function imageSelect(event) {
+    event.preventDefault()
+    if(event.target.nodeName !== "IMG") {
+        return
+    }
+
+}
+
+
+
